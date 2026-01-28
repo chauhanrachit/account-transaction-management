60a4cec1348fbe4d28609569338f4ec8ba0ea0aa
Rachit Chauhan <chauhan.rachit21@gmail.com>
2026-01-28 14:41:34 +0530

    Initial project skeleton and configuration

---
+++ src/test/java/com/company/atms/AccountTransactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.atms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTransactionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
